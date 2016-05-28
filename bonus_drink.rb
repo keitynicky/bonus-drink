d3e36350acd5321ce536eb45df2177be41236b1c
nana <keyaki.436vrwgz@gmail.com>
2016-05-28 13:42:07 +0900

    動作確認後コミット

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,12 @@
 class BonusDrink
+  BONUS_COUNT = 3
   def self.total_count_for(amount)
-    0
+    all_empty_count = amount
+    while all_empty_count >= BONUS_COUNT do
+      new_drink_count, all_empty_count = all_empty_count.divmod(BONUS_COUNT)
+      all_empty_count += new_drink_count
+      amount += new_drink_count
+    end
+    return amount
   end
 end
